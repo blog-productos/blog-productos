b632ec13cd6afde3c3596fe31799336c4a061e9d
DESKTOP-VGGUGDD\PABEV <pablofullstack@protonmail.com>
2023-08-12 13:20:01 +0200

    Modificar puerto

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', ''),
+            'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'blog-productos'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
