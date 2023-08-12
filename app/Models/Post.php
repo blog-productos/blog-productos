ee5f7c0e229d586a372fec62d48c1591bf0de493
Anja-FullStackDev <anja.rubio@hotmail.com>
2023-08-12 17:55:16 +0200

    Modulos
    Incorporar modelos

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //Relacion uno a muchos inversa
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    
+    public function category (){
+        return $this->belongsTo(Cartegory::class);
+    }
+
+    //Relaacion muchos a muchos
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relacion uno a uno polimorfica
+
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
