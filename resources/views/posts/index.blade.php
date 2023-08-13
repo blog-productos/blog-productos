211029304aba5c61996624558705ccd178239b53
Anja-FullStackDev <anja.rubio@hotmail.com>
2023-08-13 16:56:17 +0200

    creacion controller
    creacion controller

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="contaniner ml-auto mr-auto bg-red-500 ">
+        Mi blog
+    </div>
+</x-app-layout>
+
+    
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PostController::class, 'index'])->name('posts.index');
 
 Route::middleware([
     'auth:sanctum',
