88c3d9adfac88643271764132497f570ad5f2d67
DESKTOP-VGGUGDD\PABEV <pablofullstack@protonmail.com>
2023-08-15 18:24:01 +0200

    Instalación AdminLTE 3, creación HomeController y la vista para el panel de administrador.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+@section('title', 'Blog Productos')
+
+@section('content_header')
+    <h1>Blog Productos</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido al panel de administrador de Blog Productos</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
